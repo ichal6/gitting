8bb59312721b5f5990b09ab3a031c3e76b24c806
Darek_Bobek <darbob080@gmail.com>
2019-07-23 09:51:20 +0200

    change introduce

--- gittie.py
+++ gittie.py
@@ -1,12 +1,9 @@
 def introduce():
-    print("Hello, I'm Gittie!")
+    print("Hello, I'm Gittie is great day!")
 
-<<<<<<< HEAD
 def joke()
     print('My boss told me to have a good day.. so I went home.')
-=======
 
 def add(a, b):
     c = a + b
     return c
->>>>>>> 837b5a77fd2e9cdc124e35e936b29be2fe36f772
