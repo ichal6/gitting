c6b92022f258b5f3b247eee60e6c26afdc9cfb71
Darek_Bobek <darbob080@gmail.com>
2019-07-23 10:00:40 +0200

    add shout

--- gittie.py
+++ gittie.py
@@ -4,9 +4,11 @@ def introduce():
     print("Hello, I'm a Monster")
 >>>>>>> ec8baa2667f5960bd7c6fb7a8e4261f484470dad
 
-def joke()
+def joke():
     print('My boss told me to have a good day.. so I went home.')
 
 def add(a, b):
     c = a + b
     return c
+def shout():
+    print('cokolwiek')
